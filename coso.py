2a95f8098d6a3d73bd64ac8769fa8b51092cfff9
iglesiasfacu <iglesiasfacu14@gmail.com>
2020-08-19 00:44:25 -0300

    '19/8'

--- coso.py
+++
@@ -1,8 +0,0 @@
-club = input('Cual es el club mas grande de la Argentina?: ')
-dato = ['river', 'River', 'river plate', 'RIVER', 'River Plate', 'CARP', 'carp']
-
-if club in dato:
-    print('Bien ahi crack ;)')
-else:
-    print('Incorrecto rey')
-    print('Pista: le gano la final mas importante de la historia a su clasico rival.')
